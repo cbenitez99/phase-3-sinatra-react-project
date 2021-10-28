d49b1f59e62c403d293656013f174d43793c08e4
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-10-28 16:31:25 -0500

    made get routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/" do
-    Game.all.to_json(:include => :platform)
+    "Go to http://localhost:9292/games"
   end
 
-  get "/games" do
+  get "/games" do #gives all games
     Game.all.to_json
   end
 
-  get "/reviews" do
-    Review.all.to_json#(:include => :game, :except => :game_id)
+  get "/reviews" do #gives all reviews
+    Review.all.to_json
   end
 
-  get "/platforms" do 
-    Platform.all.to_json
+  get "/games/:id" do #gives back a specific game
+   game = Game.find(params[:id])
+   game.to_json
   end
+
+  get "/reviews/:id" do #gives back a specific review
+    review = Review.all.find(params[:id])
+    review.to_json
+  end
+
+  get "/games/:id/review" do #gives back a certain games review
+    game = Game.find(params[:id])
+    game.to_json(:include => :reviews)
+   end
+
 end
