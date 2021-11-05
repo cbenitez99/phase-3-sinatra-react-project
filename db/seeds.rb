fa84645264cc579e5e86cf99941c4be3bd34d0df
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-11-04 19:44:25 -0500

    fixed typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ puts "🌱 Seeding data..."
 Game.destroy_all
 Review.destroy_all
 
-game1 = Game.create(title: "Battlefeild V", price: "30", platform: "Xbox One")
+game1 = Game.create(title: "Battlefield V", price: "30", platform: "Xbox One")
 game2 = Game.create(title: "Call of Duty: World at War", price: "60", platform: "PC")
 
 review1 = Review.create(user_name: "Alex", content: "Awesome game to play with friends!", game_id: game1.id)
